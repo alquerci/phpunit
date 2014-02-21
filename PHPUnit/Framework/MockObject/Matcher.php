709fa9ba6d7da48853da077ad003573e646bb518
alquerci <alquerci@email.com>
2014-02-21 05:19:59 +0100

    [MockObject] Fixed wrong method call

--- PHPUnit/Framework/MockObject/Matcher.php
+++ PHPUnit/Framework/MockObject/Matcher.php
@@ -302,7 +302,7 @@ class PHPUnit_Framework_MockObject_Matcher implements PHPUnit_Framework_MockObje
 
                 $this->methodNameMatcher->toString(),
                 $this->invocationMatcher->toString(),
-                $e->getDescription()
+                $e->getMessage()
               )
             );
         }
